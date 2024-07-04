b33363bb18c5df3754ace146c6917ad422f7e438
Juan Ramos Diaz (EXT-Nokia) <juan.ramos_diaz.ext@nokia.com>
2024-07-04 16:39:39 +0200

    updated instructions of cloudbook components in bat file

--- cb_start.bat
+++ cb_start.bat
@@ -5,9 +5,9 @@ set modo=1
 set /p "modo=Bienvenido a Cloudbook, pulsa enter para continuar, cualquier otra tecla para modo discreto: "
 
 :: A continuacion introduce las rutas correctas de tu programa (ojo, entre comillas dobles)
-set ruta_deployer_launcher="C:\Users\jramosdi\Desktop\Juan\SkyNNet\sk_cloudbook\cloudbook_deployer"
-set ruta_agent="C:\Users\jramosdi\Desktop\Juan\SkyNNet\sk_cloudbook\cloudbook_agent"
-set ruta_maker="C:\Users\jramosdi\Desktop\Juan\SkyNNet\sk_cloudbook\cloudbook_maker2"
+set ruta_deployer_launcher="C:\Users\jramosdi\Proyectos\SkyNNet\sk_cloudbook\cloudbook_deployer"
+set ruta_agent="C:\Users\jramosdi\Proyectos\SkyNNet\sk_cloudbook\cloudbook_agent"
+set ruta_maker="C:\Users\jramosdi\Proyectos\SkyNNet\sk_cloudbook\cloudbook_maker2"
 
 :: Info, a continuacion los valores de colores cantosos y discretos
 set deployer_cantoso=c0
@@ -24,23 +24,29 @@ set maker_discreto=09
 set nl=^& echo.
 set instrucciones_deployer=Bienvenido al deployer^
 %nl%======================^
-%nl%	Pendiente terminar instrucciones^
-%nl%	py cloudbook_deployer.py
+%nl%	Uso:^
+%nl%	py cloudbook_deployer.py -project_folder nombre_proyecto [-fast_start]^
+%nl%    ^
+%nl%	Para uso avanzado ejecuta py cloudbook_deployer.py y se mostraran opciones avanzadas
 
 set instrucciones_launcher=Bienvenido al launcher^
 %nl%======================^
-%nl%	Pendiente terminar instrucciones^
-%nl%	py cloudbook_launch.py
+%nl%	Uso:^
+%nl%	py cloudbook_run.py -project_folder nombre_proyecto
 
 set instrucciones_agent=Bienvenido al agente^
 %nl%====================^
-%nl%	Pendiente terminar instrucciones^
-%nl%	py gui.py
+%nl%	Uso:^
+%nl%	py gui.py (Lanza interfaz gráfica)^
+%nl%    ^
+%nl%	Para uso avanzado del agente ejecuta py agent.py para ver las instrucciones
 
 set instrucciones_maker=Bienvenido al maker^
 %nl%===================^
-%nl%	Pendiente terminar instrucciones^
-%nl%	py cloudbook_maker.py
+%nl%	Uso:^
+%nl%	py cloudbook_maker.py -project_folder nombre_proyecto^
+%nl%    ^
+%nl%	Para uso avanzado del agente ejecuta py cloudbook_maker.py para ver las instrucciones
 
 
  
